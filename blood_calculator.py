e559fc348ca0e3d01cb6960a9affac5036828960
David Ward <david.a.ward@duke.edu>
2024-01-19 12:22:58 -0500

    Fix bug where program ends with any entry

--- blood_calculator.py
+++ blood_calculator.py
@@ -2,9 +2,10 @@ def interface():
     print("Blood Calculator")
     print("Enter which test you want?")
     print("9 - Quit")
-    choice = input("Enter your choice:")
-    if choice == "9":
-        return
+    while True:
+        choice = input("Enter your choice:")
+        if choice == "9":
+            return
         
         
 interface()
