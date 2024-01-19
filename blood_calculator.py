f8fee713310f5def19ec4108e28307823bea3ca8
David Ward <david.a.ward@duke.edu>
2024-01-19 12:18:13 -0500

    Designed basic text interface

---
+++ blood_calculator.py
@@ -0,0 +1,10 @@
+def interface():
+    print("Blood Calculator")
+    print("Enter which test you want?")
+    print("9 - Quit")
+    choice = input("Enter your choice:")
+    if choice == "9":
+        return
+        
+        
+interface()
