451430e2e18290ab8d1113825d1628b7b9ffe66e
David Ward <david.a.ward@duke.edu>
2024-01-23 17:29:02 -0500

    Added LDL analysis to program

--- blood_calculator.py
+++ blood_calculator.py
@@ -2,6 +2,7 @@ def interface():
     print("Blood Calculator")
     print("Enter which test you want?")
     print("1 - HDL")
+    print("2 - LDL")
     print("9 - Quit")
     while True:
         choice = input("Enter your choice:")
@@ -9,6 +10,8 @@ def interface():
             return
         elif choice == "1":
             HDL_driver()
+        elif choice == "2":
+            LDL_driver()
  
  
 def HDL_driver():
@@ -32,6 +35,30 @@ def analyse_HDL(HDL_value):
         
 def output_HDL_result(HDL_level):
     print("The characterization of HDL is {}".format(HDL_level))
+
+def LDL_driver():
+    print("LDL")
+    LDL = get_LDL_input()
+    LDL_level = analyse_LDL(LDL)
+    output_LDL_result(LDL_level)
+        
+def get_LDL_input():
+    LDL_value = input("Enter an LDL result: ")
+    LDL_value = int(LDL_value)
+    return LDL_value
+    
+def analyse_LDL(LDL_value):
+    if LDL_value < 130:
+        return "Normal"
+    elif 130 <= LDL_value < 160:
+        return "Borderline High"
+    elif 160 <= LDL_value < 190:
+        return "High"
+    else:
+        return "Very High"
+        
+def output_LDL_result(LDL_level):
+    print("The characterization of LDL is {}".format(LDL_level))
 
        
     
