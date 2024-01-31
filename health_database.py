9de51706b1dfde27676334c133ed67fb6cadcd26
David Ward <david.a.ward@duke.edu>
2024-01-31 12:08:11 -0500

    Start a list database

--- health_database.py
+++ health_database.py
@@ -8,8 +8,12 @@ def create_database_entry(name, age, mrn):
     
 
 def main():
-    x = create_database_entry("Ann Ables", 36, 101)
-    print(x)
+    db = []
+    db.append(create_database_entry("Ann Ables", 36, 101))
+    db.append(create_database_entry("Bob Boyles", 25, 102))
+    db.append(create_database_entry("Chris Chou", 50, 103))
+    
+    print(db)
     
     
 if __name__ == "__main__":
