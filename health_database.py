a09ecc757aa2e498639a7f569414f637f48ab491
David Ward <david.a.ward@duke.edu>
2024-02-09 10:11:46 -0500

    Final changes to health_database so it will run with dictionary

--- health_database.py
+++ health_database.py
@@ -2,7 +2,7 @@
 Single patient record:  {"First Name": str,
                          "Last Name": str,
                          "Age": int,
-                         "MRN": int, 
+                         "MRN": int,
                          "Tests": list]
 Test list:  [("HDL", 100), ("LDL", 15), ("HR", 62)]
 """
@@ -13,14 +13,14 @@ def create_database_entry(first_name, last_name, age, mrn):
                    "Last Name": last_name,
                    "Age": age,
                    "MRN": mrn,
-                   "Tests": []]
+                   "Tests": []}
     return new_patient
 
 
 def get_full_name(patient):
     full_name = "{} {}".format(patient["First Name"], patient["Last Name"])
     return full_name
-    
+
 
 def print_database(db):
     for patient in db:
@@ -44,15 +44,15 @@ def add_test_to_patient(db, MRN, test_name, test_value):
 
 def main():
     db = []
-    db.append(create_database_entry("Ann Ables", 36, 101))
-    db.append(create_database_entry("Bob Boyles", 25, 102))
-    db.append(create_database_entry("Chris Chou", 50, 103))
+    db.append(create_database_entry("Ann", "Ables", 36, 101))
+    db.append(create_database_entry("Bob", "Boyles", 25, 102))
+    db.append(create_database_entry("Chris", "Chou", 50, 103))
     print(db)
-    print(id(db[0]))
     print()
     add_test_to_patient(db, 101, "HDL", 100)
     add_test_to_patient(db, 101, "LDL", 102)
     print(db)
+    print()
     print_database(db)
 
 
