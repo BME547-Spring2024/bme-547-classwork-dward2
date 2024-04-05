e453bff4f47fe1c5c2c8270f21adafe60eaef094
David Ward <david.a.ward@duke.edu>
2024-04-05 10:35:10 -0400

    Improved layout and added combobox for donation center choice.

--- gui.py
+++ gui.py
@@ -54,53 +54,71 @@ def main():
         image_label.configure(image=tk_image)
         image_label.image = tk_image
 
+    # Create root window
     root = tk.Tk()
     root.title("Blood Donor Database")
-    root.geometry("1000x800")
+    # root.geometry("1000x800")
 
+    # Create title, name and id widgets
     title_label = ttk.Label(root, text="Blood Donor Database")
-    title_label.grid(column=0, row=0)
-    other_label = ttk.Label(root, text="Name:")
-    other_label.grid(column=0, row=1, sticky=tk.E)
+    title_label.grid(column=0, row=0, columnspan=2, sticky=tk.W)
+    name_label = ttk.Label(root, text="Name:")
+    name_label.grid(column=0, row=1, sticky=tk.E)
     name_data = tk.StringVar()
     # name_data.set("Enter your name here")
     name_entry = ttk.Entry(root, textvariable=name_data)
-    name_entry.grid(column=1, row=1)
+    name_entry.grid(column=1, row=1, sticky=tk.W)
+
+    # Create label to use for GUI output
     result_label = ttk.Label(root)
     result_label.grid(column=1, row=5)
 
+    # Create widgets for rH factor
     rh_value = tk.StringVar()
     rh_value.set("-")
     rh_checkbox = ttk.Checkbutton(root, text="rH positive", onvalue="+",
                                   offvalue="-", variable=rh_value)
     rh_checkbox.grid(column=1, row=4)
 
+    # Create widgets for blood type letter
     blood_type = tk.StringVar()
     a_btn = ttk.Radiobutton(root, text="A", variable=blood_type, value="A")
-    a_btn.grid(column=0, row=3)
+    a_btn.grid(column=0, row=3, sticky=tk.W)
     b_btn = ttk.Radiobutton(root, text="B", variable=blood_type, value="B")
-    b_btn.grid(column=0, row=4)
+    b_btn.grid(column=0, row=4, sticky=tk.W)
     ab_btn = ttk.Radiobutton(root, text="AB", variable=blood_type, value="AB")
-    ab_btn.grid(column=0, row=5)
+    ab_btn.grid(column=0, row=5, sticky=tk.W)
     o_btn = ttk.Radiobutton(root, text="O", variable=blood_type, value="O")
-    o_btn.grid(column=0, row=6)
+    o_btn.grid(column=0, row=6, sticky=tk.W)
 
+    # Create Combobox Widgets
+    dc_label = ttk.Label(root, text="Closest Donation Center")
+    dc_label.grid(column=2, row=0, sticky=tk.W, padx=5)
+    dc_choice = tk.StringVar()
+    dc_locations = ("Durham", "Orange", "Wake")
+    dc_entry = ttk.Combobox(root, values=dc_locations,
+                            textvariable=dc_choice, state='readonly')
+    dc_entry.grid(column=2, row=1, sticky=tk.W, padx=5)
 
-    cancel_btn = ttk.Button(root, text="Cancel", command=cancel_btn_cmd)
-    cancel_btn.grid(column=2, row=6)
-    ok_btn = ttk.Button(root, text="Ok", command=ok_btn_cmd)
-    ok_btn.grid(column=1, row=6)
-
+    # Create image widgets
     filename = "avatar.jpg"
     pil_image = load_and_size_image(filename)
     tk_image = ImageTk.PhotoImage(pil_image)
     image_label = ttk.Label(root, image=tk_image)
     image_label.image= tk_image
-    image_label.grid(column=3, row=0)
+    image_label.grid(column=3, row=0, rowspan=5)
 
     image_select_btn = ttk.Button(root, text="Select Picture",
                                   command=image_select_cmd)
     image_select_btn.grid(column=4, row=0)
+
+
+    # Create button widgets
+    cancel_btn = ttk.Button(root, text="Cancel", command=cancel_btn_cmd)
+    cancel_btn.grid(column=2, row=6)
+    ok_btn = ttk.Button(root, text="Ok", command=ok_btn_cmd)
+    ok_btn.grid(column=1, row=6)
+
 
     root.mainloop()
     print("Finished")
